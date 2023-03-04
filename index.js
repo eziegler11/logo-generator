0c39686cf4015ecddb2345a7bfc1e804fca5f241
erichziegler <erich.ziegler11@gmail.com>
2023-03-04 11:07:01 -0700

    Have prompts configured and user input is creating the SVG file.

--- index.js
+++ index.js
@@ -5,49 +5,53 @@ const fs = require('fs');
 // Array of prompts to generate logo
 const questions = [
 	{
-		type: 'input',
-		message: 'What is the title of your project?',
-		name: 'title',
+		type: 'input', // needs to be up to only 3 characters max
+		message: 'What letters do you want in your logo?',
+		name: 'text',
 	},
 	{
 		type: 'input',
-		message: 'What is your project about?',
-		name: 'description',
-	},
-	{
-		type: 'input',
-		message: 'How does a user install your code?',
-		name: 'installation',
-	},
-	{
-		type: 'input',
-		message: 'How does a user run your code?',
-		name: 'usage',
-	},
-	{
-		type: 'input',
-		message: 'How can a user contribute to your code?',
-		name: 'contribute',
-	},
-	{
-		type: 'input',
-		message: 'How can a user test your code?',
-		name: 'test',
+		message: 'What color do you want the text to be?',
+		name: 'textColor',
 	},
 	{
 		type: 'list',
-		message: 'Which license do you want to use?',
-		name: 'license',
-		choices: ['MIT', 'GNU', 'CC0', 'BSD'],
+		message: 'Which shape do you want to use?',
+		name: 'shape',
+        choices: ['circle', 'triangle', 'square'],
 	},
 	{
 		type: 'input',
-		message: 'What is your GitHub username?',
-		name: 'github',
-	},
-	{
-		type: 'input',
-		message: 'What is your email address?',
-		name: 'email',
+		message: 'What color do you want the shape to be?',
+		name: 'shapeColor',
 	},
 ];
+
+// Writes the SVG file and displays success, or shows error
+function writeToFile(fileName, data) {
+	fs.writeFile(fileName, data, (err) =>
+		err ? console.error(err) : console.log('Generated logo.svg')
+	);
+}
+
+// Initializes the app when run in the command line and produces the content of the ReadMe
+function init() {
+	inquirer.prompt(questions).then((response) => {
+		console.log(response);
+		const logo = `<svg version="1.1"
+        width="300" height="200"
+        xmlns="http://www.w3.org/2000/svg">
+   
+     <circle cx="150" cy="100" r="80" fill="${response.shapeColor}"/>
+   
+     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${response.textColor}">${response.text}</text>
+   
+   </svg>`;
+
+		writeToFile('logo.svg', logo);
+	});
+}
+
+// Function call to initialize app
+init();
+
